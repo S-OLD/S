a75abfb4c465c1a20b901d4f6ea8f0ebb85e7cf0
S-OLD <shenaittt8@gmail.com>
2026-05-13 11:49:46 +0500

    Update s.py

--- s.py
+++ s.py
@@ -460,7 +460,7 @@ def BNG_71_():
             continue
 
 def menu_clear():
-    sys.stdout.write("\033[20;0H")
+    sys.stdout.write("\033[35;0H")
     sys.stdout.write("\033[J")
     sys.stdout.flush()
 def old_clone():
